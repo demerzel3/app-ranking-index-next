80ab771e22eae9c0bfe847e42b9e7e1919d62cb6
Gabriele Genta <gabriele.genta@gmail.com>
2023-04-07 16:28:08 +0200

    Ignore pg-native at bundler level

--- next.config.js
+++ next.config.js
@@ -1,7 +1,15 @@
+const webpack = require('webpack');
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
     appDir: true,
+  },
+  webpack: (config) => {
+    return {
+      ...config,
+      plugins: [...config.plugins, new webpack.IgnorePlugin({ resourceRegExp: /^pg-native$/ })],
+    };
   },
 };
 
